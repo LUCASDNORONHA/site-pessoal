d5525793e2b09edaa2317841a198066698239ce0
Lucas Dias Noronha <lucasdiasnoronha1@gmail.com>
2024-07-15 15:48:00 -0300

    Primeira versão

--- js/mathjax-config.js
+++ js/mathjax-config.js
@@ -1,6 +1,6 @@
 window.MathJax = {
   CommonHTML: { linebreaks: { automatic: true } },
   tex2jax: { inlineMath: [ ['$', '$'], ['\\(','\\)'] ], displayMath: [ ['$$','$$'], ['\\[', '\\]'] ], processEscapes: false },
-  TeX: { noUndefined: { attributes: { mathcolor: 'red', mathbackground: '#FFEEEE', mathsize: '90%' } } },
+  TeX: { noUndefined: { attributes: { mathcolor: 'blue', mathbackground: '#FFEEEE', mathsize: '90%' } } },
   messageStyle: 'none'
 };
