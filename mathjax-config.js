4e67a8b5eca0028433a1295243def77ccfc23874
Lucas Dias Noronha <107411935+LUCASDNORONHA@users.noreply.github.com>
2025-05-25 19:28:51 -0300

    Delete mathjax-config.js

--- mathjax-config.js
+++
@@ -1,6 +0,0 @@
-window.MathJax = {
-  CommonHTML: { linebreaks: { automatic: true } },
-  tex2jax: { inlineMath: [ ['$', '$'], ['\\(','\\)'] ], displayMath: [ ['$$','$$'], ['\\[', '\\]'] ], processEscapes: false },
-  TeX: { noUndefined: { attributes: { mathcolor: 'blue', mathbackground: '#FFEEEE', mathsize: '90%' } } },
-  messageStyle: 'none'
-};
